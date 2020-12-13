96341cf898bd1147fba153652a8cc4b02febf329
Phan <nghiemphan93@gmail.com>
2020-12-13 23:36:27 +0100

    demo Login Logout

--- frontend/src/app/auth/auth.module.ts
+++ frontend/src/app/auth/auth.module.ts
@@ -1,10 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { AuthRoutingModule } from './auth-routing.module';
-import { LoginComponent } from './login/login.component';
+import {AuthRoutingModule} from './auth-routing.module';
+import {LoginComponent} from './login/login.component';
 import {ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
+import {MatButtonModule} from "@angular/material/button";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
 
 
 @NgModule({
@@ -13,7 +16,11 @@ import {HttpClientModule} from "@angular/common/http";
     CommonModule,
     AuthRoutingModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
   ]
 })
-export class AuthModule { }
+export class AuthModule {
+}
--- frontend/src/app/auth/login/login.component.ts
+++ frontend/src/app/auth/login/login.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Observable} from "rxjs";
 import {FormControl, FormGroup} from "@angular/forms";
-import {HttpClient} from "@angular/common/http";
+import {AuthService} from "../../services/auth.service";
 
 @Component({
   selector: 'app-login',
@@ -9,47 +9,35 @@ import {HttpClient} from "@angular/common/http";
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  data$ = new Observable<string>();
+  isAuth$ = new Observable<boolean>();
   loginForm = new FormGroup({
     username: new FormControl(''),
     password: new FormControl('')
   });
 
-  constructor(private http: HttpClient) {
+  constructor(private authService: AuthService) {
   }
 
   ngOnInit() {
-    this.data$ = this.http.get<string>('http://localhost:5005/getdatademo');
+    this.isAuth$ = this.authService.getAuth();
   }
 
-  onSubmit(): void {
-    const formData = new FormData();
-    formData.append('username', this.loginForm.value.username);
-    formData.append('password', this.loginForm.value.password);
-
-    this.http.post('http://localhost:5005/login', formData).subscribe(result => {
-      if (result.toString().includes('Goldene Drachen') || result.toString().includes('Order,Date,Postcode')) {
-        console.log('logged in successful');
-        this.data$ = this.http.get<string>('http://localhost:5005/getdatademo');
-      } else {
-        console.log('logged in FAILED...');
-      }
-    });
+  async onLogin(): Promise<void> {
+    try {
+      await this.authService.login(this.loginForm.value.username, this.loginForm.value.password);
+    } catch (e) {
+      console.log(e);
+      alert(`${e.statusText} ${e.message}`);
+    }
   }
 
-  onLogOut(): void {
-    console.log('loggin out');
-    this.http.get('http://localhost:5005/logout').subscribe(result => {
-      if (result.toString().includes('Not a member yet? Join Takeaway.com')) {
-        console.log('logged out successful');
-      } else {
-        console.log('logged out FAILED');
-      }
-    });
-  }
-
-  onGetData(): void {
-    this.data$ = this.http.get<string>('http://localhost:5005/getdatademo');
+  async onLogOut(): Promise<void> {
+    try {
+      await this.authService.logout();
+    } catch (e) {
+      console.log(e);
+      alert(`${e.statusText} ${e.message}`);
+    }
   }
 
 }
