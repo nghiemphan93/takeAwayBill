337dfc4cbe033e0389b7aa890b9c609227f4c884
Phan <nghiemphan93@gmail.com>
2020-12-13 23:47:10 +0100

    updated @angular/cli

--- frontend/src/app/services/order.service.ts
+++ frontend/src/app/services/order.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable, of} from "rxjs";
 import {Order} from "../models/Order";
+import {catchError} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
