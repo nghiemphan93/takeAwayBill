6c0606da5b230163cec6f72f8abb26e1029f0f9b
Duc <viet.vu001@stud.fh-dortmund.de>
2020-12-16 12:51:12 +0100

    changed default page to 'Login page'

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)},
   {path: 'login', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)},
   {path: '**', loadChildren: () => import('./error/error.module').then(m => m.ErrorModule)},
