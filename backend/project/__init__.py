6b7e25abebdecf04a108c178ed2f8e8a311e0716
Nghiem Phan <nghiem.phan@app-pilots.de>
2021-05-18 19:58:21 +0200

    fixed /getOrderByDate with new token

--- backend/project/__init__.py
+++ backend/project/__init__.py
@@ -2,6 +2,7 @@ import datetime
 import io
 import uuid
 from datetime import timedelta
+from threading import Thread
 from typing import Dict
 
 import pandas as pd
@@ -90,11 +91,13 @@ def logout():
 def initAuth():
     token = request.headers.get('token')
     currentUser = sessions.get(token)
+    'https://restaurant-portal-api.takeaway.com/api/restaurant/orders?period_type=week&year=2021&number=19'
     url = f'https://restaurants-old.takeaway.com/orders/archive?csv&period=week&date_end={datetime.datetime.now().date().isoformat()}'
     if currentUser:
         if currentUser.isExpired():
             sessions.pop(token)
             return jsonify(message='not authenticated'), 401
+        # TODO: set Bearer Token to each request
         result = currentUser.getSession().get(url)
         if 'Join Takeaway.com!' not in result.text:
             return jsonify(message='authenticated'), 200
@@ -106,7 +109,7 @@ def initAuth():
 @cross_origin()
 def getOrdersByDate():
     token = request.headers.get('token')
-    currentUser = sessions.get(token)
+    # currentUser = sessions.get(token)
 
     date = request.form.get('date')
     sortDirection = request.form.get('sortDirection')
@@ -118,25 +121,72 @@ def getOrdersByDate():
     if tempDate.dayofweek == 6:
         tempDate = tempDate + timedelta(days=1)
     tempDate = pd.to_datetime(tempDate, format='%Y-%m-%d')
-    tempDate = f'{tempDate.year}-{tempDate.month:02d}-{tempDate.day:02d}'
+    year = tempDate.date().year
+    weekNumber = tempDate.isocalendar()[1]
 
-    if currentUser:
-        if currentUser.isExpired():
-            sessions.pop(token)
-            return jsonify(message='not authenticated'), 401
-        result = currentUser.getSession().get(
-            f'https://restaurants-old.takeaway.com/orders/archive?csv&period=week&date_end={tempDate}')
-        if 'Order,Date,Postcode' in result.text:
-            billsDf: pd.DataFrame = pd.read_csv(io.StringIO(result.content.decode('utf-8')))
-            billsDf['Total amount'] = billsDf['Total amount'].str.replace(',', '.')
-            billsDf['Total amount'] = billsDf['Total amount'].astype(float)
-            billsDf['Paid online'] = billsDf['Paid online'].fillna(0)
-            billsDf['Date'] = pd.to_datetime(billsDf['Date'], format='%d-%m-%Y %H:%M')
-            billsDf = billsDf.loc[billsDf['Date'].dt.day == pd.to_datetime(date, format='%Y-%m-%d').day]
-            billsDf = billsDf.rename(
-                columns={'Date': 'createdAt', 'Order': 'orderCode', 'Postcode': 'postcode', 'Total amount': 'price',
-                         'Paid online': 'paidOnline'})
-            billsDf = billsDf.sort_values(by=sortColumn, ascending=True and sortDirection == 'asc')
-            return jsonify(billsDf.to_dict(orient='records')), 200
-    else:
-        return jsonify(message='not authenticated'), 401
+    result = requests.get(
+        f'https://restaurant-portal-api.takeaway.com/api/restaurant/orders?period_type=week&year={year}&number={weekNumber}',
+        headers={"Authorization": f'Bearer {token}'})
+    totalPages = result.json().get('meta').get('total_pages')
+
+    # combine all dfs
+    threads = []
+    for page in range(1, totalPages + 1):
+        threads.append(ThreadWithReturnValue(target=createSingleDf, args=(token, year, weekNumber, page,)))
+        threads[page - 1].start()
+
+    dfs = []
+    for thread in threads:
+        df = thread.join()
+        if df is not None:
+            dfs.append(df)
+    print('finito')
+    billsDf = pd.concat(dfs)
+    print(billsDf)
+
+    # billsDf: pd.DataFrame = pd.DataFrame(result.json().get('data').get('orders'))
+    billsDf['Total amount'] = billsDf['amount'].str.replace(',', '.')
+    billsDf['Total amount'] = billsDf['Total amount'].astype(float)
+    billsDf['Paid online'] = billsDf['paid_online'].fillna(False)
+    billsDf['Date'] = pd.to_datetime(billsDf['date'], format='%d-%m-%Y %H:%M:%S')
+
+    print(f'date from frontend: {date}')
+    print(f'date from frontend: {pd.to_datetime(date, format="%Y-%m-%d").day}')
+    print(f'date from backend: {billsDf["Date"].dt.day}')
+
+    billsDf = billsDf.loc[billsDf['Date'].dt.day == pd.to_datetime(date, format='%Y-%m-%d').day]
+    billsDf = billsDf.rename(
+        columns={'Date': 'createdAt', 'code': 'orderCode', 'city': 'postcode', 'Total amount': 'price',
+                 'Paid online': 'paidOnline'})
+    billsDf['paidOnline'] = billsDf['paidOnline'].apply(lambda x: 1 if x == True else 0)
+
+    billsDf = billsDf[['createdAt', 'orderCode', 'postcode', 'price', 'paidOnline']]
+    billsDf = billsDf.sort_values(by=sortColumn, ascending=True and sortDirection == 'asc')
+
+    print(billsDf)
+    return jsonify(billsDf.to_dict(orient='records')), 200
+    # else:
+    #     return jsonify(message='not authenticated'), 401
+
+
+class ThreadWithReturnValue(Thread):
+    def __init__(self, group=None, target=None, name=None,
+                 args=(), kwargs={}):
+        Thread.__init__(self, group, target, name, args, kwargs)
+        self._return = None
+
+    def run(self):
+        if self._target is not None:
+            self._return = self._target(*self._args,
+                                        **self._kwargs)
+
+    def join(self, *args):
+        Thread.join(self, *args)
+        return self._return
+
+
+def createSingleDf(token: str, year: int, weekNumber: int, page: int) -> pd.DataFrame:
+    result = requests.get(
+        f'https://restaurant-portal-api.takeaway.com/api/restaurant/orders?period_type=week&year={year}&number={weekNumber}&page={page}',
+        headers={"Authorization": f'Bearer {token}'})
+    return pd.DataFrame(result.json().get('data').get('orders'))
