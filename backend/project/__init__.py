2c26738bb2fd9c09a65c58646179d5575b19b710
Nghiem Phan <nghiem.phan@app-pilots.de>
2021-05-19 22:08:24 +0200

    fixed backend endpoints with new api from lieferando, added multi threading to quickly call all orders in a week then filtered

--- backend/project/__init__.py
+++ backend/project/__init__.py
@@ -1,72 +1,35 @@
-import datetime
-import io
-import uuid
 from datetime import timedelta
 from threading import Thread
-from typing import Dict
 
 import pandas as pd
 import requests
 from flask import Flask, jsonify, request
 from flask_cors import cross_origin
-from requests import Session
-
-
-class User:
-    def __init__(self, token: str, createdAt: datetime, session: Session,
-                 expireTime: timedelta = timedelta(hours=3)):
-        self.token = token
-        self.createdAt = createdAt
-        self.session = session
-        self.expireTime = expireTime
-
-    def getCreatedAt(self) -> datetime:
-        return self.createdAt
-
-    def getExpireTime(self) -> datetime:
-        return self.expireTime
-
-    def getSession(self) -> Session:
-        return self.session
-
-    def isExpired(self) -> bool:
-        return datetime.datetime.now() - self.getCreatedAt() > self.getExpireTime()
-
 
 app = Flask(__name__)
-sessions: Dict[str, User] = {}
 
 
 @app.route("/", methods=['GET'])
 @cross_origin()
 def helloWorld():
-    print(sessions)
     return jsonify(message='server works...')
 
 
 @app.route("/login", methods=['POST'])
 @cross_origin()
 def login():
-    print('logging in....')
-
     username = request.form.get('username')
     password = request.form.get('password')
     currentSession = requests.Session()
-    result = currentSession.post("https://restaurant-portal-api.takeaway.com/api/login",
-                                 data={
-                                     'username': username,
-                                     'password': password,
-                                 })
-    # print(result.json())
-    # print(type(result.json()))
+    result = requests.post("https://restaurant-portal-api.takeaway.com/api/login",
+                           data={
+                               'username': username,
+                               'password': password,
+                           })
     response = result.json()
 
     if response.get('code') == 200:
-        # newToken = result.json().get('data').get('access_token')
         newToken = response.get('data').get('access_token')
-        sessions[newToken] = User(token=newToken,
-                                  createdAt=datetime.datetime.now(),
-                                  session=currentSession)
         return jsonify(message='logged in successfully', token=newToken), 200
     else:
         return jsonify(message='logged in unsuccessfully'), 401
@@ -76,40 +39,18 @@ def login():
 @cross_origin()
 def logout():
     token = request.headers.get('token')
-    currentUser = sessions.get(token)
-    if currentUser:
-        result = currentUser.getSession().post("https://restaurants-old.takeaway.com/logout")
-        if 'Join Takeaway.com!' in result.text:
-            sessions.pop(token)
+    try:
+        requests.post("https://restaurant-portal-api.takeaway.com/api/logout",
+                      headers={"Authorization": f'Bearer {token}'})
         return jsonify(message='logged out successfully'), 200
-    else:
+    except Exception:
         return jsonify(message='logged out unsuccessfully'), 500
-
-
-@app.route("/initAuth", methods=['GET'])
-@cross_origin()
-def initAuth():
-    token = request.headers.get('token')
-    currentUser = sessions.get(token)
-    'https://restaurant-portal-api.takeaway.com/api/restaurant/orders?period_type=week&year=2021&number=19'
-    url = f'https://restaurants-old.takeaway.com/orders/archive?csv&period=week&date_end={datetime.datetime.now().date().isoformat()}'
-    if currentUser:
-        if currentUser.isExpired():
-            sessions.pop(token)
-            return jsonify(message='not authenticated'), 401
-        # TODO: set Bearer Token to each request
-        result = currentUser.getSession().get(url)
-        if 'Join Takeaway.com!' not in result.text:
-            return jsonify(message='authenticated'), 200
-    else:
-        return jsonify(message='not authenticated'), 401
 
 
 @app.route("/getOrdersByDate", methods=['POST'])
 @cross_origin()
 def getOrdersByDate():
     token = request.headers.get('token')
-    # currentUser = sessions.get(token)
 
     date = request.form.get('date')
     sortDirection = request.form.get('sortDirection')
@@ -140,11 +81,8 @@ def getOrdersByDate():
         df = thread.join()
         if df is not None:
             dfs.append(df)
-    print('finito')
     billsDf = pd.concat(dfs)
-    print(billsDf)
 
-    # billsDf: pd.DataFrame = pd.DataFrame(result.json().get('data').get('orders'))
     billsDf['Total amount'] = billsDf['amount'].str.replace(',', '.')
     billsDf['Total amount'] = billsDf['Total amount'].astype(float)
     billsDf['Paid online'] = billsDf['paid_online'].fillna(False)
@@ -158,15 +96,12 @@ def getOrdersByDate():
     billsDf = billsDf.rename(
         columns={'Date': 'createdAt', 'code': 'orderCode', 'city': 'postcode', 'Total amount': 'price',
                  'Paid online': 'paidOnline'})
-    billsDf['paidOnline'] = billsDf['paidOnline'].apply(lambda x: 1 if x == True else 0)
+    billsDf['paidOnline'] = billsDf['paidOnline'].apply(lambda x: 1 if x is True else 0)
 
     billsDf = billsDf[['createdAt', 'orderCode', 'postcode', 'price', 'paidOnline']]
     billsDf = billsDf.sort_values(by=sortColumn, ascending=True and sortDirection == 'asc')
 
-    print(billsDf)
     return jsonify(billsDf.to_dict(orient='records')), 200
-    # else:
-    #     return jsonify(message='not authenticated'), 401
 
 
 class ThreadWithReturnValue(Thread):
@@ -180,7 +115,7 @@ class ThreadWithReturnValue(Thread):
             self._return = self._target(*self._args,
                                         **self._kwargs)
 
-    def join(self, *args):
+    def join(self, *args) -> pd.DataFrame:
         Thread.join(self, *args)
         return self._return
 
