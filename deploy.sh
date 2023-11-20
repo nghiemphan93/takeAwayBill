c93fb64a93878fa344f757d8661a59a08f5142bb
Nghiem Phan <069784@hdi.de>
2023-11-20 16:45:46 +0100

    ""

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,4 @@
 
 scp ./k8s/*.yaml nghiemphan.de:~/projects/takeawaybill/
 ssh nghiemphan.de "kubectl apply -f ~/projects/takeawaybill/"
+#docker build -t docker.nghiemphan.de/admin/takeawaybill-backend:latest -f ./backend/Dockerfile .
